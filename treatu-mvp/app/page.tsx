989d97c133f7963f479c478971b75e9c057d97cf
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-07-12 15:44:25 +0200

    initial-setup-done

--- treatu-mvp/app/page.tsx
+++ treatu-mvp/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Welcome to the App Router!</h1>
       <p className="mt-4 text-lg text-gray-700">Edit <code>app/page.tsx</code> to get started.</p>
+            <Button>Test Button</Button>
+            <Input></Input>
     </main>
   );
 }
