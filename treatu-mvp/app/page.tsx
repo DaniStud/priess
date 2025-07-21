6296f4c9cd1a33f9a5f8051a4e69f8dfa125962b
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-07-21 19:41:29 +0200

    login-form-basic-styling

--- treatu-mvp/app/page.tsx
+++ treatu-mvp/app/page.tsx
@@ -12,19 +12,29 @@ export default function Home() {
       <main className="">
         <Navigation />
         
-        <section className="card">
-        <Button >Lorem</Button>
-        <h2>KBH's SELFCARE PLATFORM</h2>
-        <h2>Welcome Back!</h2>
-          <h1>Log ind</h1>
-          <BusinessLoginForm />
-          <p>eller</p>
-          <a>Tilmeld din virksomhed i dag!</a>
-          <div>
-            <a href="#">Instagram</a>
-            <a href="#">Facebook</a>
-            <a href="#">Web?</a>
+        <section>
+          <div className="hidden sm:block">
+            <h2>KBH's SELFCARE PLATFORM</h2>
+            <h2>Welcome Back!</h2>
           </div>
+
+            <div className="card p-10 pb-20 rounded-lg mt-[10vh] ml-[5vw] max-w-[90vw]">
+            <h1 className="mb-32">Log ind</h1>
+            <BusinessLoginForm />
+            <div className="flex items-center my-6">
+              <hr className="flex-1 border-t border-gray-300" />
+              <span className="mx-4 text-gray-500">eller</span>
+              <hr className="flex-1 border-t border-gray-300" />
+            </div>
+            <Button variant="outline" className="w-full mb-6 bg-gray-200 text-gray-700 hover:bg-gray-300" asChild>
+              <a href="/signup">Tilmeld din virksomhed i dag!</a>
+            </Button>
+            <div className="flex justify-between mt-10">
+              <a href="#">Instagram</a>
+              <a href="#">Facebook</a>
+              <a href="#">Web?</a>
+            </div>
+            </div>
         </section>
       </main>
     </>
